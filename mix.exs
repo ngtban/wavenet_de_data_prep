7b501e156117861b29e69ea95ceda74e8141422b
Ban Ngụy / Bernard Woolley <ban.nguy@tutanota.com>
2022-09-25 15:45:18 +0700

    fix: bump version, bump dependencies, adjust README (#15)

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule DataPrepration.MixProject do
   def project do
     [
       app: :data_prepration,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: "0.1.1",
+      elixir: "~> 1.14.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,11 +24,11 @@ defmodule DataPrepration.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ecto_sql, "~> 3.7.0"},
-      {:postgrex, "~> 0.15.10"},
-      {:jason, "~> 1.2"},
-      {:jaxon, "~> 2.0"},
-      {:nimble_parsec, "~> 1.0"}
+      {:ecto_sql, "~> 3.8.3"},
+      {:postgrex, "~> 0.16.5"},
+      {:jason, "~> 1.4.0"},
+      {:jaxon, "~> 2.0.8"},
+      {:nimble_parsec, "~> 1.2.3"}
     ]
   end
 end
